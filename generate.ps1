a012a709201194248443cbaa347f691b00a769ba
martin-costello <martin.costello@just-eat.com>
2024-06-29 12:38:35 +0100

    Simplify regeneration
    Separate generation from updating.

--- generate.ps1
+++ generate.ps1
@@ -1,27 +1,36 @@
 #! /usr/bin/env pwsh
 
+param(
+    [Parameter(Mandatory = $false)][switch] $Regenerate
+)
+
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
 $env:KIOTA_TUTORIAL_ENABLED = "false"
 
 $OpenApiUrl = "https://raw.githubusercontent.com/martincostello/api/dotnet-nightly/src/API/wwwroot/swagger/api/openapi.json"
+$OutputPath = "./Client"
 
 Push-Location ./KiotaExperiment
 
 try {
-    kiota generate `
-        --additional-data false `
-        --class-name ApiClient `
-        --clean-output `
-        --deserializer Microsoft.Kiota.Serialization.Json.JsonParseNodeFactory `
-        --exclude-backward-compatible `
-        --language CSharp `
-        --namespace-name KiotaExperiment.Client `
-        --openapi $OpenApiUrl `
-        --output ./Client `
-        --serializer Microsoft.Kiota.Serialization.Json.JsonSerializationWriterFactory `
-        --structured-mime-types "application/json"
+    if ($Regenerate) {
+        kiota generate `
+            --additional-data false `
+            --class-name ApiClient `
+            --clean-output `
+            --deserializer Microsoft.Kiota.Serialization.Json.JsonParseNodeFactory `
+            --exclude-backward-compatible `
+            --language CSharp `
+            --namespace-name KiotaExperiment.Client `
+            --openapi $OpenApiUrl `
+            --output $OutputPath `
+            --serializer Microsoft.Kiota.Serialization.Json.JsonSerializationWriterFactory `
+            --structured-mime-types "application/json"
+    } else {
+        kiota update --clean-output --output $OutputPath
+    }
 
     if ($LASTEXITCODE -eq 0) {
         dotnet build
